83081f5cf29669c9f9c8ba855ec57243bbfc578f
lynnmuthoni <lynnmuthoni00@gmail.com>
2023-07-28 07:04:07 -0700

    changes

---
+++ assets/php/connection.php
@@ -0,0 +1,28 @@
+<?php
+ $myPDO = new PDO('sqlite:/home/Ronnie/projects.db');
+
+
+// function display_recipe(){
+        
+//     $con = mysqli_connect("localhost", "root", "", "cookbook");
+//     $product = "'pizza'"; //just a placeholder
+//     $sql = "SELECT * FROM product WHERE name = $product";
+//     $res = mysqli_query($con,$sql);
+
+//     while($row = mysqli_fetch_assoc($res)) {
+//         $name = $row['name'];
+//         $description = $row['description'];
+//         $date = $row['date'];
+            
+//         echo $name;
+//         echo "<br>";
+//         echo $description;
+//         echo "<br>";
+//         echo $date;
+//         echo "<br>";
+//     }
+//     mysqli_close($con);
+// }
+
+// display_recipe();
+?>
